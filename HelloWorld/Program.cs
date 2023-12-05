0e2c9f7702b088cdb2a31447a567a0cccdfc974b
Szymon <Szymon@DESKTOP-QVDOT5D>
2023-12-05 22:54:34 +0100

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string imie = "Szymon";
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Nazywam się Szymon Chyc.");
+
+
+        }
+    }
+}
